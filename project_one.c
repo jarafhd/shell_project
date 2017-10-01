efa3a8b5a5e68d6f0e8e3e6d2386e8964135c0af
GunsofBRIXTON89 <joseaordaz@gmail.com>
2017-10-01 01:37:10 -0600

    Update project_one.c

--- project_one.c
+++ project_one.c
@@ -16,6 +16,33 @@
 #include<stdlib.h>
 
 #define MAX_LINE 80 /* The maximum lenth command */
+#define MAX_HISTORY_SIZE 10 /* History buffer size */
+
+
+void 	printHistory(char* history_buffer[],int h,int flag){
+	int i;
+	if(flag == 0){ /* array has not looped around */
+	printf("in flag 0\n");	
+	for(i = h-1; i >= 0; --i){
+			printf("%d %s \n", i+1, history_buffer[i]);
+		}
+	}
+	else if(flag == 1){
+		printf("in flag 1\n");
+		if(h == 0){ h = 10;}
+		for(i= h-1;i >= 0;--i){
+			printf("%d %s \n", i+10, history_buffer[i]);
+
+		}
+		for(i = MAX_HISTORY_SIZE - 1; i >= h; --i){
+			printf("%d %s \n", i, history_buffer[i]);
+		}
+	}
+}/* end of print_history */
+
+
+/* === Main ====================================================================================
+===============================================================================================*/
 
 int main(void)
 {
@@ -24,20 +51,39 @@ int should_run = 1; /*flag to determine when to exit program */
 pid_t pid;
 char input_buffer[MAX_LINE];
 char* token;
-int i=0;
+int i;
+char* history_buffer[MAX_HISTORY_SIZE];
+int h = 0; /* h denotes the mosts current element in the the history array */
+int flag = 0; /* flag denotes where or not history array has(1) or hasn't(0) wrapped around */
+
+
 	while (should_run) {
 	printf("osh> ");
 	fflush(stdout);
 	fgets(input_buffer,MAX_LINE + 1, stdin);
-	token = strtok(input_buffer," ");
+	printf("h is %d\n",h);
+	strcpy(history_buffer[h],input_buffer);
+	//strlen(*history_buffer[h])
+	token = strtok(input_buffer,"\n");
+	token = strtok(token," ");
  	for(i = 0; token != NULL; ++i){
-	 args[i] = token;
-         printf(" 1) %s\n",args[i]);
-	 token = strtok(NULL," ");
+		args[i] = token;
+        	printf(" %d) %s\n", i, args[i]);
+		token = strtok(NULL," ");
 	}
 	args[i] = NULL;
+
+	if(strcmp(args[0],"exit") == 0 ){
+		printf("Exiting OSC Shell\n");
+		should_run = 0;
+		break;
+	
+	}else if(strcmp(args[0],"history") ==  0){
+	printHistory(history_buffer,h,flag);
+	}
+
 	/* After reading user input, the steps are:*/
-	printf("forking child now\n");	
+	printf("forking child now\n");
 	/* (1) fork a child process using fork()*/
 	pid = fork();
 	/* (2) the child process will invoke execvp()*/
@@ -47,28 +93,33 @@ int i=0;
 		exit(1);
 	}
 	else if(pid == 0){
-	        printf("In Child:%s\n",args[0]);
-       		printf("In Child:%s\n",args[1]);
-       		printf("In Child:%s\n",args[2]);
-		printf("i = %d\n", i);
-		if(execvp(*args[0],args) == -1) {
-		printf("ERROR: execvp() Failed!\n");
-		exit(1);
+	       	printf("In Child: %s\n", args[0]);
+		printf("In Child: %s\n", args[1]);
+		printf("In Child: %s\n", args[2]);
+		if(execvp(args[0],args) == -1) {
+			printf("ERROR: execvp() Failed!\n");
+			exit(1);
 		}
 		printf("Clean Child Exit\n");
 		exit(0);
+	}else if(h < 10){
+		++h;
+	}else if(h == 10){
+		h = 0;
+		flag = 1;
 	}else{
 		printf("\n");
 		/* (3) if command included &, parent will invoke wait()*/
-		if(args[i-1] == "&"){
-	  	wait();
+		if(strcmp(args[i-1], "&") == 0){
+			wait(NULL);
 	 	}
-		wait(); 
+		// wait(NULL);
 		printf("Parent Exiting\n");
 
-	 }
-
+	}
 	} /* end of while(should_run) */
 
 	return 0;
 }
+
+
