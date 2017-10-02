22c57874b7de1521abae9ec768b990cc9458711d
jarafhd <lcplom@hotmail.com>
2017-10-02 09:50:21 -0700

    commented, run to compile and run

--- project_one.sh
+++ project_one.sh
@@ -1,3 +1,9 @@
+//created by Jose Ordaz
+//9-29-2017
+//file name project_one.sh
+//run this shell script to compile the file project_one.c written as c code and run the program
+//run by using ./project_one.sh
+
 #!/bin/bash
 
 gcc -c project_one.c 
